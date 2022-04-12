4ebc1fd6dc0d39b83a29750fce217aba6e9f65ae
OkothOnyango <okoth.f.onyango@gmail.com>
2022-04-12 14:16:53 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -17,17 +17,12 @@ int main(void)
 		int ch;
 
 
+		for (ch = 'a'; ch <= 'z'; ch++)
 
-			for (ch = 'a'; ch <= 'z'; ch++)
-
-						putchar(ch);
-
-				for (ch = 'A'; ch <= 'Z'; ch++)
-
-							putchar(ch);
-
-					putchar('\n');
-
-						return (0);
+			putchar(ch);
+		for (ch = 'A'; ch <= 'Z'; ch++)
+			putchar(ch);
+		putchar('\n');
+		return (0);
 
 }
